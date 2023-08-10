d882b297da9f7bf750445a08f687dad846916b7e
Mateja Musa <matejamusa6@gmail.com>
2023-08-10 19:38:00 +0200

    implement chatting functionality

--- src/main/java/com/matejamusa/chat/config/WebSocketEventListener.java
+++ src/main/java/com/matejamusa/chat/config/WebSocketEventListener.java
@@ -1,8 +1,13 @@
 package com.matejamusa.chat.config;
 
+import com.matejamusa.chat.chat.ChatMessage;
+import com.matejamusa.chat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +16,18 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User Disconnected: {}", username);
+            ChatMessage chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
